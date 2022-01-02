74ac81a3a80e8bfa05848aaa40ad3115398926f9
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2022-01-02 11:22:28 -0500

    Changed template recipe

--- src/components/App.js
+++ src/components/App.js
@@ -70,25 +70,19 @@ export default function App() {
 const sampleRecipes = [
   {
     id: 1,
-    name: 'Plain Chicken',
-    cookTime: '1:46',
+    name: "Mark's Kickin' Classic Breast of Chicken",
+    cookTime: '20 minutes @ 425°',
     servings: 3,
     instructions:
-      '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
+      '1.) Preheat oven to 425°F/220°C (200°C fan).\n2.) Pound chicken to 1.5cm / 0.6" at the thickest part - using a rolling pin, meat mallet or even your fist (key tip for even cooking + tender chicken).\n3.) Mix Seasoning.\n4.) Line tray with foil and baking / parchment paper. Place chicken upside down on tray. Drizzle chicken with about 1 tsp oil. Rub over with fingers. Sprinkle with Seasoning.\n5.) Flip chicken. Drizzle with 1 tsp oil, rub with fingers, sprinkle with Seasoning, covering as much of the surface area as you can.\n6.) Bake 18 minutes, or until surface is golden per photos and video, or internal temperature is 165°F/75°C using a meat thermometer.\n7.) Remove from oven and immediately transfer chicken to serving plates.\n8.) Wait 3 - 5 minutes before serving, garnished with freshly chopped parsley if desired. Pictured with a side of Garlic Butter Rice with Kale.',
     ingredients: [
-      { id: 1, name: 'Chicken', amount: '2 Pounds' },
-      { id: 2, name: 'Salt', amount: '1 Tbs' }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Plain Pork',
-    cookTime: '0:45',
-    servings: 5,
-    instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork',
-    ingredients: [
-      { id: 1, name: 'Pork', amount: '3 Pounds' },
-      { id: 2, name: 'Paprika', amount: '2 Tbs' }
+      { id: 1, name: 'Chicken Breast (6 0z.)', amount: '4' },
+      { id: 2, name: 'Olive Oil', amount: '2 Tsp' },
+      { id: 3, name: 'Salt', amount: '1/2 Tsp' },
+      { id: 4, name: 'Pepper', amount: '1/2 Tsp' },
+      { id: 5, name: 'Brown Sugar', amount: '1 1/2 Tbsp' },
+      { id: 6, name: 'Dried Oregano or Thyme', amount: '1 Tsp' },
+      { id: 7, name: 'Garlic Powder', amount: '1/4 Tsp' }
     ]
   }
 ]

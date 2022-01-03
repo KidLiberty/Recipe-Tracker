380e9cf4eb97dbfbbccca430d59d321343287927
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2022-01-03 11:58:08 -0500

    Added search bar to Recipe List

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,15 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import Recipe from './Recipe'
 import { RecipeContext } from './App'
 
 export default function RecipeList({ recipes }) {
+  const [searchedRecipe, setSearchedRecipe] = useState()
   const { handleRecipeAdd } = useContext(RecipeContext)
+  const input = document.querySelector('#search')
+
   return (
     <div className='recipe-list'>
+      <input
+        type='text'
+        name='search'
+        id='search'
+        placeholder='Search...'
+        className='recipe-edit__input'
+        onChange={e => setSearchedRecipe(e.target.value.toLowerCase())}
+      />
       <div>
-        {recipes.map(recipe => {
-          return <Recipe key={recipe.id} {...recipe} />
-        })}
+        {input.value !== ''
+          ? recipes
+              .filter(recipe =>
+                recipe.name.toLowerCase().includes(searchedRecipe.toLowerCase())
+              )
+              .map(recipe => {
+                return <Recipe key={recipe.id} {...recipe} />
+              })
+          : recipes.map(recipe => {
+              return <Recipe key={recipe.id} {...recipe} />
+            })}
       </div>
       <div className='recipe-list__add-recipe-btn-container'>
         <button className='btn btn--primary' onClick={handleRecipeAdd}>

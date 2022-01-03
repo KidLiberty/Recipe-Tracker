07e60f4c3aa83fb02542bac78cc64009409a5ccc
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2022-01-03 12:29:22 -0500

    Search functionality fix

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,11 +1,11 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useRef, useContext } from 'react'
 import Recipe from './Recipe'
 import { RecipeContext } from './App'
 
 export default function RecipeList({ recipes }) {
-  const [searchedRecipe, setSearchedRecipe] = useState()
+  const [searchedRecipe, setSearchedRecipe] = useState('')
   const { handleRecipeAdd } = useContext(RecipeContext)
-  const input = document.querySelector('#search')
+  const myRef = useRef()
 
   return (
     <div className='recipe-list'>
@@ -15,12 +15,22 @@ export default function RecipeList({ recipes }) {
         id='search'
         placeholder='Search...'
         className='recipe-edit__input'
-        onChange={e => setSearchedRecipe(e.target.value.toLowerCase())}
+        value={searchedRecipe}
+        ref={myRef}
+        onChange={e => setSearchedRecipe(e.target.value)}
       />
       <div>
-        {recipes.map(recipe => {
-          return <Recipe key={recipe.id} {...recipe} />
-        })}
+        {searchedRecipe !== ''
+          ? recipes
+              .filter(recipe =>
+                recipe.name.toLowerCase().includes(searchedRecipe.toLowerCase())
+              )
+              .map(recipe => {
+                return <Recipe key={recipe.id} {...recipe} />
+              })
+          : recipes.map(recipe => {
+              return <Recipe key={recipe.id} {...recipe} />
+            })}
       </div>
       <div className='recipe-list__add-recipe-btn-container'>
         <button className='btn btn--primary' onClick={handleRecipeAdd}>

a3804cea8f5f61cfbb0aec99b43f90428e5eeb8e
Brandon Stone <brandonstone@macbook-pro.myfiosgateway.com>
2022-01-22 01:06:21 -0500

    Test

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -33,7 +33,7 @@ export default function RecipeList({ recipes }) {
       </div>
       <div className='recipe-list__add-recipe-btn-container'>
         <button className='btn btn--primary' onClick={handleRecipeAdd}>
-          Add Recipe
+          Add New Recipe
         </button>
       </div>
     </div>
